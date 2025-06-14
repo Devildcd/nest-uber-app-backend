285768c5385d0636158200acfba5bb4319d1f44a
Devildcd <caneladavid19@gmail.com>
2025-06-14 16:33:20 -0500

    feat(api): integrate Swagger UI with OpenAPI spec

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerService } from './docs/swagger/swagger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swagger = app.get(SwaggerService);
+  swagger.setup(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
