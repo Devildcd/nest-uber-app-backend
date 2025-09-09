10f34ea761e9edb0cea02eef10be868a075ffc7c
jennyinfante86 <jinfante86@gmail.com>
2025-09-09 19:03:01 +0200

    feat(transantions, orders): Add Transactions and Orders entities and module structure
    This commit introduces the foundation for the new Transactions and Orders features, including its entity and dto.
    
    - *Transaction:* Creates the Transaction entity.
    - *Orders:* Creates the Order entity.

---
+++ src/modules/orders/dto/create-orders.dto.spec.ts
@@ -0,0 +1,7 @@
+import { CreateOrdersDto } from './create-orders.dto';
+
+describe('CreateOrdersDto', () => {
+  it('should be defined', () => {
+    expect(new CreateOrdersDto()).toBeDefined();
+  });
+});
---
+++ src/modules/orders/dto/update-orders.dto.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateOrdersDto } from './update-orders.dto';
+
+describe('UpdateOrdersDto', () => {
+  it('should be defined', () => {
+    expect(new UpdateOrdersDto()).toBeDefined();
+  });
+});
